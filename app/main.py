0b5c921c84d67cba848eebd0a5a8a505ecd42f69
Bernard <bsuwirjo@gmail.com>
2025-03-21 14:43:15 -0700

    Updated api route to be /api/___

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Email List API")
 
-@app.post("/add_emails", status_code=200)
+@app.post("/api/add_emails", status_code=200)
 def add_email(email_data: schemas.EmailBase, db: Session = Depends(get_db)):
     if "+" in email_data.email:
         raise HTTPException(status_code=500, detail="Email cannot contain '+' character")
@@ -28,7 +28,7 @@ def add_email(email_data: schemas.EmailBase, db: Session = Depends(get_db)):
     db.commit()
     return {"message": "Email added successfully"}
 
-@app.get("/get_emails", response_model=schemas.EmailList)
+@app.get("/api/get_emails", response_model=schemas.EmailList)
 def get_emails(project_name: str = Query(..., description="Name of the project to get emails for"), db: Session = Depends(get_db)):
     emails = db.query(models.Email).filter(
         models.Email.project_name == project_name
